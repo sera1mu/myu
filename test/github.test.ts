3d18e2ba1bb60cbc7cefe4d8bf5c4c689487f64f
sera1mu <ifrea0281@gmail.com>
2021-04-01 13:54:39 +0900

    test(github): Add github test

---
+++ test/github.test.ts
@@ -0,0 +1,119 @@
+import { assertEquals, assertThrowsAsync } from 'asserts';
+import { createRepoUrl, getRepoTags } from '../util/github.ts';
+
+let responseStatus = {
+  code: 200,
+  text: 'OK',
+};
+
+const fetcher = async function (
+  input: string | Request | URL,
+  init?: RequestInit | undefined,
+): Promise<Response> {
+  const content = JSON.stringify([
+    {
+      name: 'v1.0.0',
+      commit: {
+        sha: 'thisIsMock',
+        url: 'thisIsMock',
+      },
+      zipball_url: 'thisIsMock',
+      tarball_url: 'thisIsMock',
+      node_id: 'thisIsMock',
+    },
+    {
+      name: 'v1.1.0',
+      commit: {
+        sha: 'thisIsMock',
+        url: 'thisIsMock',
+      },
+      zipball_url: 'thisIsMock',
+      tarball_url: 'thisIsMock',
+      node_id: 'thisIsMock',
+    },
+    {
+      name: 'v1.1.2',
+      commit: {
+        sha: 'thisIsMock',
+        url: 'thisIsMock',
+      },
+      zipball_url: 'thisIsMock',
+      tarball_url: 'thisIsMock',
+      node_id: 'thisIsMock',
+    },
+  ]);
+
+  return new Response(content, {
+    status: responseStatus.code,
+    statusText: responseStatus.text,
+  });
+};
+
+Deno.test('GitHub API: Check created URL correctly', () => {
+  const result = createRepoUrl(
+    'https://api.github.com',
+    'denoland',
+    'deno',
+    'tags',
+  );
+
+  assertEquals('https://api.github.com/repos/denoland/deno/tags', result);
+});
+
+Deno.test('GitHub API: Check got repository tags correctly', async () => {
+  responseStatus = {
+    code: 200,
+    text: 'OK',
+  };
+
+  const tags = await getRepoTags(
+    'https://api.github.com',
+    'denoland',
+    'deno',
+    fetcher,
+  );
+
+  assertEquals([
+    {
+      name: 'v1.0.0',
+      commit: { sha: 'thisIsMock', url: 'thisIsMock' },
+      zipball_url: 'thisIsMock',
+      tarball_url: 'thisIsMock',
+      node_id: 'thisIsMock',
+    },
+    {
+      name: 'v1.1.0',
+      commit: { sha: 'thisIsMock', url: 'thisIsMock' },
+      zipball_url: 'thisIsMock',
+      tarball_url: 'thisIsMock',
+      node_id: 'thisIsMock',
+    },
+    {
+      name: 'v1.1.2',
+      commit: { sha: 'thisIsMock', url: 'thisIsMock' },
+      zipball_url: 'thisIsMock',
+      tarball_url: 'thisIsMock',
+      node_id: 'thisIsMock',
+    },
+  ], tags);
+});
+
+Deno.test('GitHub API: Check throwed error when error code is invalid', async () => {
+  responseStatus = {
+    code: 404,
+    text: 'Not Found',
+  };
+
+  assertThrowsAsync(
+    async () => {
+      await getRepoTags(
+        'https://api.github.com',
+        'denoland',
+        'deno',
+        fetcher,
+      );
+    },
+    undefined,
+    'Failed to fetch repository tags: 404 Not Found',
+  );
+});
