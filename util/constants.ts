3eb67b6ece0bf3f896221c163af650edd54cc4c9
sera1mu <ifrea0281@gmail.com>
2021-04-01 13:54:55 +0900

    chore(util): Add constants.ts

---
+++ util/constants.ts
@@ -0,0 +1,19 @@
+/**
+ * Myu version
+ */
+export const VERSION = '0.1.0';
+
+/**
+ * GitHub API origin
+ */
+export const GITHUB_API_ORIGIN = 'https://api.github.com';
+
+/**
+ * Deno repository owner name
+ */
+export const DENO_REPO_OWNER = 'denoland';
+
+/**
+ * Deno repository name
+ */
+export const DENO_REPO_NAME = 'deno';
