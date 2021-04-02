8f031d7300e4d73dd07c6bd5ec4445cf1b0f62b8
sera1mu <ifrea0281@gmail.com>
2021-04-02 17:55:38 +0900

    test(github): Add channel URL check

--- test/github.test.ts
+++ test/github.test.ts
@@ -7,9 +7,9 @@ let responseStatus = {
 };
 
 // deno-lint-ignore require-await
-const fetcher = async function (
-  input: string | Request | URL,
-  init?: RequestInit | undefined,
+const fetchMock = async function (
+  _input: string | Request | URL,
+  _init?: RequestInit | undefined,
 ): Promise<Response> {
   const content = JSON.stringify([
     {
@@ -60,16 +60,30 @@ Deno.test('GitHub API: Check created URL correctly', () => {
   assertEquals('https://api.github.com/repos/denoland/deno/tags', result);
 });
 
+Deno.test('GitHub API: Check created URL correctly #2', () => {
+  const result = createRepoUrl(
+    'denolib',
+    'awesome-deno',
+    'releases',
+  );
+
+  assertEquals(
+    'https://api.github.com/repos/denolib/awesome-deno/releases',
+    result,
+  );
+});
+
 Deno.test('GitHub API: Check got repository tags correctly', async () => {
   responseStatus = {
     code: 200,
     text: 'OK',
   };
 
+  window.fetch = fetchMock;
+
   const tags = await getRepoTags(
     'denoland',
     'deno',
-    fetcher,
   );
 
   assertEquals([
@@ -103,12 +117,13 @@ Deno.test('GitHub API: Check throwed error when error code is invalid', () => {
     text: 'Not Found',
   };
 
+  window.fetch = fetchMock;
+
   assertThrowsAsync(
     async () => {
       await getRepoTags(
         'denoland',
         'deno',
-        fetcher,
       );
     },
     undefined,
